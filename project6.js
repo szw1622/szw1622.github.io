c0254584283f6f14c2598258ebde1d80183a572e
szw1622 <u1313034@utah.edu>
2023-12-28 18:40:09 -0700

    upload

---
+++ project6.js
@@ -0,0 +1,136 @@
+var raytraceFS = `
+struct Ray {
+	vec3 pos;
+	vec3 dir;
+};
+
+struct Material {
+	vec3  k_d;	// diffuse coefficient
+	vec3  k_s;	// specular coefficient
+	float n;	// specular exponent
+};
+
+struct Sphere {
+	vec3     center;
+	float    radius;
+	Material mtl;
+};
+
+struct Light {
+	vec3 position;
+	vec3 intensity;
+};
+
+struct HitInfo {
+	float    t;
+	vec3     position;
+	vec3     normal;
+	Material mtl;
+};
+
+uniform Sphere spheres[ NUM_SPHERES ];
+uniform Light  lights [ NUM_LIGHTS  ];
+uniform samplerCube envMap;
+uniform int bounceLimit;
+
+bool IntersectRay( inout HitInfo hit, Ray ray );
+
+// Shades the given point and returns the computed color.
+vec3 Shade( Material mtl, vec3 position, vec3 normal, vec3 view )
+{
+	vec3 color = vec3(0,0,0);
+    for ( int i=0; i<NUM_LIGHTS; ++i ) {
+		Ray shadowRay;
+		HitInfo shadowHit;
+		vec3 L = normalize(lights[i].position - position);
+		shadowRay.dir = L;
+		shadowRay.pos = position;
+
+		if (!IntersectRay(shadowHit, shadowRay)){
+			// Diffuse component
+			float diff = max(dot(normal, L), 0.0);
+	
+			// Specular component
+			float spec = pow(max(dot(normal, normalize(L + view)), 0.0), mtl.n);
+	
+			color += lights[i].intensity * (mtl.k_d * diff + mtl.k_s * spec);
+		}
+    }
+    return color;
+}
+
+// Intersects the given ray with all spheres in the scene
+// and updates the given HitInfo using the information of the sphere
+// that first intersects with the ray.
+// Returns true if an intersection is found.
+bool IntersectRay( inout HitInfo hit, Ray ray )
+{
+    hit.t = 1e30;
+    bool foundHit = false;
+
+    for ( int i=0; i<NUM_SPHERES; ++i ) {
+		Sphere curS = spheres[i];
+        vec3 oc = ray.pos - curS.center;
+        float a = dot(ray.dir, ray.dir);
+        float b = 2.0 * dot(oc, ray.dir);
+        float c = dot(oc, oc) - curS.radius * curS.radius;
+        float d = b * b - 4.0 * a * c;
+
+        if (d >= 0.0) {
+            float temp = (-b - sqrt(d)) / (2.0 * a);
+            if (temp < hit.t && temp > 0.001) {
+                hit.t = temp;
+                hit.position = ray.pos + temp * ray.dir;
+                hit.normal = normalize(hit.position - curS.center);
+                hit.mtl = curS.mtl;
+                foundHit = true;
+            }
+        }
+    }
+    return foundHit;
+}
+
+// Given a ray, returns the shaded color where the ray intersects a sphere.
+// If the ray does not hit a sphere, returns the environment color.
+vec4 RayTracer( Ray ray )
+{
+	HitInfo hit;
+	if ( IntersectRay( hit, ray ) ) {
+		vec3 view = normalize( -ray.dir );
+		vec3 clr = Shade( hit.mtl, hit.position, hit.normal, view );
+		
+		// Compute reflections
+		vec3 k_s = hit.mtl.k_s;
+		for ( int bounce=0; bounce<MAX_BOUNCES; ++bounce ) {
+			if ( bounce >= bounceLimit ) break;
+			if ( hit.mtl.k_s.r + hit.mtl.k_s.g + hit.mtl.k_s.b <= 0.0 ) break;
+			
+			Ray r;	// this is the reflection ray
+			HitInfo h;	// reflection hit info
+			
+			r.dir = normalize(ray.dir) - 2.0 * (dot(normalize(ray.dir), hit.normal)) * hit.normal;
+			r.pos = hit.position + 0.0001 * r.dir;
+			
+			if ( IntersectRay( h, r ) ) {
+				vec3 reflectedColor = Shade(h.mtl, h.position, h.normal, normalize(-r.dir));
+				clr += k_s * reflectedColor;
+
+				// TO-DO: Update the loop variables for tracing the next reflection ray
+				hit = h;
+				ray = r;
+				k_s *= h.mtl.k_s;
+				r.pos = h.position;
+				r.dir = reflect(-r.dir, h.normal);
+			} else {
+				// The refleciton ray did not intersect with anything,
+				// so we are using the environment color
+				clr += k_s * textureCube( envMap, r.dir.xzy ).rgb;
+				break;	// no more reflections
+			}
+		}
+		return vec4( clr, 1 );	// return the accumulated color, including the reflections
+	} else {
+		return vec4( textureCube( envMap, ray.dir.xzy ).rgb, 0 );	// return the environment color
+	}
+}
+`;
